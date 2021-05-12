548f2b99e963e60a948ba9452607c4507ce7e07e
Бураков Сергей Романович <bsr@college.spk>
2021-05-12 10:01:25 +0700

    my first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1 задание
+let s = 'С';
+let e = 'е';
+let r = 'р';
+let g = 'г';
+let y = 'й';
+let name = (s+e+r+g+e+y);
+console.log (name);
+// 2 задание
+let text = document.getElementsByTagName('p')[0];
+text.style.fontSize = "2.3em";
+text.style.color = 'blue';
+text.style.textAlign = 'center';
+// 3 задание
+text.textContent = 'Goodbye Sergey!';
+
+
